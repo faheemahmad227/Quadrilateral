80ab57be99aebc8ab7e24a41011885bd2296b6f0
OGharsh <72140506+OGharsh@users.noreply.github.com>
2020-10-01 00:22:35 +0530

    Create techiestalk-subscriber.py

---
+++ techiestalk-subscriber.py
@@ -0,0 +1,8 @@
+#Please Approv this Pull Request, I am Subscriber of Techies Talk
+
+print ('Python Code for Calculate the area of a parallelogram')
+
+base = float(input('Length of base: '))
+height = float(input('Measurement of height: '))
+area = base * height
+print("Area is: ", area)
